638319006d02b3fa5e03fda3dad25b4941be7da0
Chaitanya <chaitanyasharma18698@gmail.com>
2021-05-18 19:23:11 +0530

    Implement genre api

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
+const helmet=require('helmet');
 const express=require('express');
 const app=express();
 
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(helmet());
 
-
-
-
-
-
-
-
-
-
-
+app.use('/api/genres',require('./routes/genres'));
 
 
 
