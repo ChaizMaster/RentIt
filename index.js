3e2acd639237ac16026b7428a9d6fa8ca6eebf73
Chaitanya <chaitanyasharma18698@gmail.com>
2021-05-21 11:06:40 +0530

    Improve Error handling code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const mongoose=require('mongoose');
 const config=require('config');
 const Joi=require('joi');
@@ -6,7 +7,23 @@ const helmet=require('helmet');
 const express=require('express');
 const app=express();
 
+//catch uncaughtExceptions throughout the node app and handle them (Note: it only works for sync code, for promises we need other handler)
+process.on('uncaughtException',e=>{
+    //usually we will log this error to DB
+   console.log("Found an uncaught Exception in the node app",e);
+   //if there's an error we should exit the process with a non zero code and let process manager restart the process, to keep the process
+   //clean
+   process.exit(1);
+});
 
+// handle unhandledRejections throughout the node app
+process.on('unhandledRejection',e=>{
+    //usually we will log this error to DB
+    console.log("Found an unhandled Rejection in the node app",e);
+    //if there's an error we should exit the process with a non zero code and let process manager restart the process, to keep the process
+   //clean
+   process.exit(1);
+});
 // before the app starts up we want to be sure that the privatekey for jwt is present as an environmental variable
 // otherwise our authentication endpoint will fail
 
